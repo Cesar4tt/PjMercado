6aa0d6f88857f5c2e3430b1b975456f198d3608b
RuanRCA <assuncaoruan7@gmail.com>
2025-02-21 09:53:59 -0300

    update

--- ProjetoMercado/Program.cs
+++ ProjetoMercado/Program.cs
@@ -11,7 +11,7 @@ namespace ProjetoMercado
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmMercado());
+            Application.Run(new frmLogin());
         }
     }
 }
